a06a22e6148fcbfcdaf5863afde33eb0774109f5
Simona S <simona.stasaityte@gmail.com>
2022-12-15 11:06:10 +0200

    added formik & yup to form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,12 +1,53 @@
 import css from './Form.module.css';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
 function Form(props) {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string().required(),
+      password: Yup.string().min(6).required(),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div className={css.form}>
       <h2 className={css.title}>Create Account</h2>
       <p className={css.subtitle}>Please enter your details below</p>
-      <form>
-        <input type='text' placeholder='Email' className={css.input} />
-        <input type='password' placeholder='Password' className={css.input} />
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          type='text'
+          placeholder='Email'
+          name='email'
+          className={css.input}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <p className={css.inputError}>{formik.errors.email}</p>
+        ) : (
+          ''
+        )}
+        <input
+          type='password'
+          placeholder='Password'
+          name='password'
+          className={css.input}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <p className={css.inputError}>{formik.errors.password}</p>
+        ) : (
+          ' '
+        )}
         <button type='submit' className={css.mainButton}>
           Register
         </button>
